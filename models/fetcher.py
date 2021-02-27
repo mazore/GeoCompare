13f44c9f2602d57e6ab0fcc868cbd2ee28807c1b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-27 15:24:39 -0800

    add optional cachepath option to fetcher constructor

--- models/fetcher.py
+++ models/fetcher.py
@@ -1,9 +1,15 @@
+import os
+
 class Fetcher:
-	def __init__(self, url, name):
+	def __init__(self, url, name, cachepath=None):
 		self.url = url
 		self.name = name
 		self.useragent = "GeoCompare script (https://github.com/VacFind/GeoCompare)"
 
+		if (cachepath):
+			self.cachepath = cachepath
+		else:
+			self.cachepath = os.getcwd() + "/cache"
 
 	def auth(self):
 		raise NotImplementedError()
