227c6678cdfb8ac655d94b5718757e6fc391703b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-28 12:37:57 -0800

    add id field to source class

--- models/source.py
+++ models/source.py
@@ -1,15 +1,26 @@
 class Source:
-	def __init__(self, name, fetcher_name, url):
+	def __init__(self, name, fetcher_name, url, id=None):
 		self.url = url
 		self.name = name
 		self.fetcher_name = fetcher_name
+		self.id = id
 
 	@classmethod
 	def fromDict(cls, data):
-		return cls(data['name'], data['fetcher'], data['url'])
+		try:
+			identifier = data["id"]
+		except KeyError:
+			identifier=None
+		return cls(data['name'], data['fetcher'], data['url'], id=identifier)
 
 	def fetch(self):
 		raise NotImplementedError()
 
 	def get_fetcher_name(self):
 		return self.fetcher_name
+	
+	def get_id(self):
+		return self.id
+
+	def set_id(self, id):
+		self.id = id
