d67c1bc28327390972a0aef467745819ff0e1946
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-03 15:10:14 -0800

    add some more storage tests

--- tests/modules/test_storage.py
+++ tests/modules/test_storage.py
@@ -6,6 +6,8 @@ import json
 from unittest.mock import patch
 
 class TestStorageMethods(unittest.TestCase):
+	def setUp(self):
+		self.cache = CacheEntry(Path("/some/location"), "myfile.txt", last_saved=datetime.datetime(2021,3,3,11,3,50), mkdir=False)
 
 	def test_make_cache_entry_from_filename(self):
 		filename = "2020-02-02T020202_myfile.txt" 
@@ -19,6 +21,22 @@ class TestStorageMethods(unittest.TestCase):
 		filename = "2020-02-02T020202_myfile.txt" 
 		with self.assertRaises(ValueError) as context:
 			cacheentry = CacheEntry.from_filename(filename)
+
+	def test_init_and_getters(self):
+		with patch.object(Path, 'mkdir') as mk:
+			cache = CacheEntry(Path("/some/location"), "myfile.txt", last_saved=datetime.datetime(2021,3,3,11,3,50))
+			# assert that the directory is successfully created
+			mk.assert_called_with(parents=True, exist_ok=True)
+		
+		self.assertEqual(cache.get_date_saved().time(), datetime.datetime(2021,3,3,11,3,50).time())
+		self.assertTrue(cache.get_full_path().name.endswith("myfile.txt"))
+
+
+	def test_age_at_datetime(self):
+		self.assertEqual(datetime.interval(0), self.cache.get_age_at_datetime(datetime.datetime(2021,3,3,11,3,50)))
+		self.assertEqual(datetime.interval(minutes=5), self.cache.get_age_at_datetime(datetime.datetime(2021,3,3,11,3,55)))
+		self.assertEqual(datetime.interval(minutes=-5), self.cache.get_age_at_datetime(datetime.datetime(2021,3,3,11,3,45)))
+
 		
 
 if __name__ == '__main__':
