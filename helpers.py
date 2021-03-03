75e1756fc4f176d2a5df0b55ecc2b4166d258628
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-03 10:13:22 -0800

    add comments explaining why regular distance formula is a bad idea

--- helpers.py
+++ helpers.py
@@ -2,8 +2,12 @@ from models.storage import CacheEntry
 import requests
 import json
 
+# Since a geohash (in this implementation) is based on coordinates of longitude and latitude the distance between two geohashes reflects the distance in latitude/longitude coordinates between two points, which does not translate to actual distance, see Haversine formula. 
+# https://en.wikipedia.org/wiki/Haversine_formula
+# https://en.wikipedia.org/wiki/Geohash
 def get_distance(x1, y1, x2, y2):
-	return sqrt((y2-y1)^2+(x2-x1)^2)
+	return sqrt((y2-y1)^2+(x2-x1)^2)# no
+	# use  geopy.distance.distance() instead: https://geopy.readthedocs.io/en/stable/#module-geopy.distance
 
 def fetch_unless_cache(cachepath, url, filename, headers, force_fetch=False):
 	cache_location = CacheEntry.latest_from_directory(cachepath, pattern="*"+filename)
