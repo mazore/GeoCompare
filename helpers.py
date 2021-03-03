3820a18b6a626a810f388a4bfd2ea7f54b234542
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-03 10:12:01 -0800

    account for filenames when checking for existing cache

--- helpers.py
+++ helpers.py
@@ -6,7 +6,7 @@ def get_distance(x1, y1, x2, y2):
 	return sqrt((y2-y1)^2+(x2-x1)^2)
 
 def fetch_unless_cache(cachepath, url, filename, headers, force_fetch=False):
-	cache_location = CacheEntry.latest_from_directory(cachepath)
+	cache_location = CacheEntry.latest_from_directory(cachepath, pattern="*"+filename)
 	# if there is no existing cache, create one
 	if not cache_location:
 		cache_location = CacheEntry(cachepath, filename)
