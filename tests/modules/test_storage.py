b5de4625b0cd04d36cbd7a10132250d0a7bde228
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-03 10:55:41 -0800

    patch is_file_ so tests can be run with a nonexistent file

--- tests/modules/test_storage.py
+++ tests/modules/test_storage.py
@@ -1,13 +1,17 @@
 import unittest
 from models.storage import CacheEntry
+from pathlib import Path
 import datetime
 import json
+from unittest.mock import patch
 
 class TestStorageMethods(unittest.TestCase):
 
 	def test_make_cache_entry_from_filename(self):
 		filename = "2020-02-02T020202_myfile.txt" 
-		cacheentry = CacheEntry.from_filename(filename)
+		with patch.object(Path, 'is_file', return_value=True) as ex:
+			cacheentry = CacheEntry.from_filename(filename)
+			ex.assert_called_with()
 		self.assertEqual(cacheentry.get_date_saved().time(), datetime.datetime(year=2020, month=2, day=2, hour=2, minute=2, second=2).time())
 		self.assertEqual(cacheentry.filename, "myfile.txt")
 
