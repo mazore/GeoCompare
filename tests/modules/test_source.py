5c2a44759592db60cb75e128ba5b7b5f4a088629
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-03 10:57:55 -0800

    add id and parameters to source tests

--- tests/modules/test_source.py
+++ tests/modules/test_source.py
@@ -4,15 +4,21 @@ from models.source import Source
 class TestSourceMethods(unittest.TestCase):
 
 	def test_from_dict(self):
-		reg = Source("name", "fetcher", "https://foo.com")
+		reg = Source("id", "name", "fetcher", "https://foo.com", parameters={"test": "true"})
 		dictionary = Source.fromDict({
+			"id": "id",
 			"name": "name",
 			"fetcher": "fetcher",
-			"url": "https://foo.com"
+			"url": "https://foo.com",
+			"parameters": {
+				"test": "true"
+			}
 		})
+		self.assertEqual(reg.id, dictionary.id)
 		self.assertEqual(reg.name, dictionary.name)
 		self.assertEqual(reg.fetcher_name, dictionary.fetcher_name)
 		self.assertEqual(reg.url, dictionary.url)
+		self.assertEqual(reg.parameters["test"], dictionary.get_parameter("test"))
 
 if __name__ == '__main__':
 	unittest.main()
