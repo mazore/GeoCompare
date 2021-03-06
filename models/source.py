4b98fd50b0fcc9f2f8225ad9273d9600c493c34a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-05 20:07:34 -0800

    add parser name parameter to source

--- models/source.py
+++ models/source.py
@@ -1,10 +1,11 @@
 from map import Map
 
 class Source:
-	def __init__(self, id, name, fetcher_name, url, map, parameters={}):
+	def __init__(self, id, name, fetcher_name, parser_name, url, map, parameters={}):
 		self.url = url
 		self.name = name
 		self.fetcher_name = fetcher_name
+		self.parser_name = parser_name
 		self.id = id
 		self.parameters = parameters
 		self.map = Map(map)
@@ -13,13 +14,16 @@ class Source:
 	def fromDict(cls, data):
 		params = data.get("parameters", {})
 
-		return cls(data['id'], data['name'], data['fetcher'], data['url'], data['map'], parameters=params)
+		return cls(data['id'], data['name'], data['fetcher'], data['parser'],data['url'], data['map'], parameters=params)
 
 	def fetch(self):
 		raise NotImplementedError()
 
 	def get_fetcher_name(self):
 		return self.fetcher_name
+	
+	def get_parser_name(self):
+		return self.parser_name
 
 	def get_name(self):
 		return self.name
