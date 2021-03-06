e40d2b4505840b934f9577cf6f840f259887badc
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-05 19:41:32 -0800

    add source parameter to VaccinationLocation datatype

--- models/datatypes/vaccinationlocation.py
+++ models/datatypes/vaccinationlocation.py
@@ -1,10 +1,11 @@
 class VaccinationLocation:
-	def __init__(self, name, id, address, lat, lon):
+	def __init__(self, name, id, address, lat, lon, source=None):
 		self.name = name
 		self.id = id
 		self.address = address
 		self.lat = lat
 		self.long = lon
+		self.source = source
 
 	def get_name(self):
 		return self.name
@@ -20,3 +21,6 @@ class VaccinationLocation:
 
 	def get_long(self):
 		return self.long
+
+	def get_source(self):
+		return self.source
